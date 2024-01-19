f9b41ac8f4419bf0af0e7ee8818ffaa9d7dda363
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-01-19 16:46:04 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,3 +1,45 @@
+<?php
+    try {
+        $hostname = "localhost";
+        $dbname = "VOTE";  
+        $username = "arnau";
+        $pw = "P@ssw0rd1234";
+        $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+    } catch (PDOException $e) {
+        echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+        exit;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];  // Cambiado de "username" a "email"
+        $contraseña = $_POST["password"];
+        
+        // Cambiado "user_name" a "email"
+        $querystr = "SELECT email FROM users WHERE email = :email AND password = SHA2(:contrasena, 256)";
+        $query = $pdo->prepare($querystr);
+
+        $query->bindParam(':email', $email);  // Cambiado de ":usuario" a ":email"
+        $query->bindParam(':contrasena', $contraseña);
+
+        $query->execute();
+        
+        $filas = $query->rowCount();
+        if ($filas > 0) {
+            // Iniciar la sesión y guardar el correo electrónico en la sesión
+            session_start();
+            $_SESSION['email'] = $email;  // Cambiado de "username" a "email"
+
+            // Redirigir al usuario a index.php usando JavaScript
+            echo '<script type="text/javascript">window.location = "index.php";</script>';
+            exit;
+        } else {
+            echo "<script type='text/javascript'>alert('Correo electrónico o contraseña incorrectos');</script>";
+        }
+        unset($pdo);
+        unset($query);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -15,16 +57,16 @@
 
         <div class="containerLogin">
 
-            <form class="iniciasesionLogin">
+            <form class="iniciasesionLogin" method="post">
                 <h1>INICIA SESIÓN</h1>
                 <img class="logoLogin" src="logosinfondo.png" alt="">
                 <div class="datosUsuarioLogin">
-                    <input class="inputLoginPHP" type="text" id="username" required>
-                    <label for="username">Usuario</label>
+                    <input class="inputLoginPHP" type="email" id="email" name="email" required>  
+                    <label for="email">Correo electrónico</label>  
                 </div>
 
                 <div class="datosUsuarioLogin">
-                    <input class="inputLoginPHP" type="password" id="password" required>
+                    <input class="inputLoginPHP" type="password" id="password" name="password" required>
                     <label for="password">Contraseña</label>
                 </div>
 
