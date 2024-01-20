4b724ccc1fb1b88d80ec409bf418ab7393f57755
cl6mh7 <claudiamoyamo@gmail.com>
2024-01-20 01:45:56 +0100

    ?

---
+++ páginas/db_connection.php
@@ -0,0 +1,13 @@
+<?php 
+
+    try {
+        $hostname = "localhost";
+        $dbname = "VOTE";  
+        $username = "arnau";
+        $pw = "P@ssw0rd1234";
+        $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+    } catch (PDOException $e) {
+        echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+        exit;
+    }
+?>
